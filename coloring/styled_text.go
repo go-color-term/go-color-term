fa9235c6377b91c0588d6e23b1a84a65e016046c
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2021-03-22 20:50:30 -0300

    Added documentation headers for styled_text.go

--- coloring/styled_text.go
+++ coloring/styled_text.go
@@ -1,13 +1,19 @@
 package coloring
 
+// StyledText represents a string decorated with style attributes.
+//
+// It can be obtained from a `StyleBuilder` or a `SentenceBuilder`.
 type StyledText struct {
 	s, unformatted string
 }
 
+// String returns the string represented by this `StyledText` with
+// the corresponding style attributes applied during it's creation.
 func (st *StyledText) String() string {
 	return st.s
 }
 
+// Unformatted returns the original string without any style attributes.
 func (st *StyledText) Unformatted() string {
 	return st.unformatted
 }
