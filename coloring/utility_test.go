d27fa0fc574c29237187daf458175df65d09f78b
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2021-03-27 00:03:13 -0300

    Added copyright notice header to each source code file

--- coloring/utility_test.go
+++ coloring/utility_test.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Nelson Germán Ghezzi. All rights reserved.
+// Use of this source code is governed by a MIT license that
+// can be found in the LICENSE file.
+
 package coloring_test
 
 import (
